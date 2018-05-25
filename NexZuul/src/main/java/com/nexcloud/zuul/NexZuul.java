a449a84e547d0ff67be0af486c73ff1e3f1808af
Nelson Kim <ssipflow@gmail.com>
2018-05-25 18:42:29 +0900

    NexZuul

--- NexZuul/src/main/java/com/nexcloud/zuul/NexZuul.java
+++ NexZuul/src/main/java/com/nexcloud/zuul/NexZuul.java
@@ -1,14 +1,31 @@
 package com.nexcloud.zuul;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 public class NexZuul {
+	
+	/**
+	 * DC/OS Process Check Bit
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/check")
+	public String checkBit(HttpServletRequest request) throws Exception {
+
+		return "";
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NexZuul.class, args);
